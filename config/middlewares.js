4765f2f8e3d1a7f6d4b11256006dbae948fd5371
JonathanAlexMasc <mascarenhasjonathan@gmail.com>
2023-08-12 12:29:34 -0600

    kumbaya

---
+++ config/middlewares.js
@@ -0,0 +1,29 @@
+module.exports = [
+  'strapi::errors',
+  module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  }),
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
