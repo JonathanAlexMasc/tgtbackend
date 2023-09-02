4977dfd3090b440fa905e69fd98ba09d26e22df5
JonathanAlexMasc <mascarenhasjonathan@gmail.com>
2023-09-02 11:06:14 -0600

    sjgnsog

--- config/server.js
+++ config/server.js
@@ -1,7 +1,5 @@
 module.exports = ({ env }) => ({
   url: env('https://tgtbackend-1892941aed2f.herokuapp.com'),
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
